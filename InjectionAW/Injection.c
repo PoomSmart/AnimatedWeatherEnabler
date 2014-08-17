06e250480e58cca38a4d763a1954c20c33e226ce
PoomSmart <poomst@hotmail.com>
2014-08-17 10:28:19 +0700

    Update Injection.c

--- InjectionAW/Injection.c
+++ InjectionAW/Injection.c
@@ -10,10 +10,10 @@ CFTypeRef MGCopyAnswer(CFStringRef KeyToCopy, int __unknown);
 CFTypeRef _MGCopyAnswer(CFStringRef KeyToCopy, int __unknown)
 {
 	CFTypeRef ReturnData;
-    if (!CFStringCompare(KeyToCopy, CFSTR("ProductType"), 0))
-        return CFSTR("iPhone5,1");
-    ReturnData = MGCopyAnswer(KeyToCopy, __unknown);
-    return ReturnData;
+	if (!CFStringCompare(KeyToCopy, CFSTR("ProductType"), 0))
+		return CFSTR("iPhone5,1");
+	ReturnData = MGCopyAnswer(KeyToCopy, __unknown);
+	return ReturnData;
 }
 
 DYLD_INTERPOSE(_MGCopyAnswer, MGCopyAnswer)
